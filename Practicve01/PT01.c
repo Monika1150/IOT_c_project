9ce42ae29f9a1cfbabad875bd36d850241f6369a
Monika1150 <s6519N10001@sau.ac.th>
2023-06-15 16:26:39 +0700

    commit Veision 3

--- Practicve01/PT01.c
+++ Practicve01/PT01.c
@@ -1,44 +1,26 @@
 // โจทย์ข้อที่ 1
 #include <stdio.h>
 #include <conio.h>
-
-#define PA printf("+++++++++++++++++++++++++++++++\n");
-#define WA printf("-------------------------------\n");
-
-void main()
+#define PA printf("+++++++++++++++++++++++++\n");
+void main ()
 {
-    float widths, longs, area_square, bases, highs, area_triangle;
 
-//********************** สี่เหลี่ยม **************************************
+    float widths,longs,higths,color_fot_use;
+    PA
+    printf("     Program  Color\n");
+    PA
+    printf("Enter widths :");
+    scanf("%f",&widths);
+    printf("Enter longs :");
+    scanf("%f",&longs);
+    printf("Enter higths :");
+    scanf("%f",&higths);
+    PA
+    color_fot_use = widths*2+longs*2+higths*2;
+    color_fot_use = color_fot_use/3;
+    printf("Color for use is  :%.2fGallon",color_fot_use);
+
+
+
     
-    PA
-    printf(" Program Area Shapes \n");
-    PA
-    printf("Input widths : ");
-    scanf ("%f" , &widths );
-
-    printf("Input longs : ");
-    scanf ("%f" , &longs );
-    printf("**************************\n");
-    area_square = widths * longs;
-    printf("Square area : %.2f \n" , area_square);
-    PA
-    WA
-//*********************** สามเหลี่ยม *************************************
-
-    PA
-    printf(" Program Area Shapes \n");
-    PA
-    printf("Input bases : ");
-    scanf ("%f" , &bases );
-
-    printf("Input highs : ");
-    scanf ("%f" , &highs );
-    printf("**************************\n");
-    area_triangle = bases * highs / 2;
-    printf("Triangle area : %.2f \n" , area_triangle);
-    PA
-    WA
-    
-    getch();
 }
